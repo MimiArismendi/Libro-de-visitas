5c2e34fb6d4c58015779b5f32a232c197330c80f
noemi <noemiarismendi.cc@gmail.com>
2017-03-27 08:56:10 -0600

    Libro de visitas

---
+++ Assets/js/visitas.js
@@ -0,0 +1,48 @@
+function mostrarComentario(){
+  var nuevoComentario = document.getElementById("comentario").value;
+  document.getElementById("comentario visualizado").innerHTML = nuevoComentario;
+}
+
+function textoGrande(){
+  /*var grande = document.getElementById("comentario visualizado");
+  grande.innerHTML = "comentario visualizado.style.font-size = 20px";*/
+  document.getElementById("comentario visualizado").innerHTML = "font-size = 20px;"
+}
+
+function textoMediano(){
+
+}
+
+function textoChico() {
+
+}
+
+function colorTexto(){
+
+}
+
+function colorFondo(){
+
+}
+
+function izquierda() {
+
+}
+
+function centrar() {
+  var centrar = document.getElementById("comentario visualizado");
+  centrar.innerHTML =
+}
+
+function derecha() {
+
+}
+
+function borrar() {
+  var borrar = document.getElementById("comentario");
+  borrar.value = "";
+}
+
+function agregarImagen() {
+
+}
